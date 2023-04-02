ef118f22d2ca216227cbd6b87ade5798350ae26e
Kayla S <kaylaesmith1@gmail.com>
2023-04-02 18:06:24 +0000

    Update About page (add cards and review stars) and Contact form fields.

--- bnb/forms.py
+++ bnb/forms.py
@@ -3,7 +3,7 @@ from .models import Customer
 
 
 class CustomerForm(forms.ModelForm):
-    
+
     class Meta:
         model = Customer
         fields = "__all__"
--- bnb/models.py
+++ bnb/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(blank=True)
-    city = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField(max_length=350, blank=True)
 
     def __str__(self):
         return str(self.title)
