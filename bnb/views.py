8c8180b39358f7928558fc3e74e38d4256fad7cb
Kayla S <kaylaesmith1@gmail.com>
2023-04-03 12:32:36 +0000

    Install allauth. Add layout, information and styling of login page.

--- bnb/views.py
+++ bnb/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import generic, View
 from .forms import CustomerForm
 # from django.core.mail import send_mail, BadHeaderError
@@ -31,3 +31,22 @@ class About(generic.TemplateView):
 def create_customer(request):
     form = CustomerForm()
     return render(request, 'contact_us.html', {'form': form})
+
+# POST A REVIEW OF THE BNB - from Django blog walkthrough?
+# class PostDetail(View):
+
+#     def get(self, request, slug, *args, **kwargs):
+#         queryset = Post.objects.filter(status=1)
+#         review = get_object_or404(queryset, slug=slug).order_by('created_on')
+#         liked = False
+#         if post.likes.filter(id=self.request.user.id).exists():
+#             liked = True
+
+#         return render (
+#             request,
+#             "about.html",
+#             {
+#                 "review": review,
+#                 "liked": liked
+#             },
+#         )
