d2f1ae81aaab174d2806e31400863e4b23eee3e5
Kayla S <kaylaesmith1@gmail.com>
2023-04-11 21:17:05 +0000

    Create and link breakfast page

--- bnb/urls.py
+++ bnb/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('contact/', views.Contact.as_view(), name='contact_us'),
     path('about/', views.About.as_view(), name='about'),
     path('contact/', create_customer, name='contact_us'),
+    path('breakfast/', views.Breakfast.as_view(), name='breakfast'),
     # path('myaccount/', views.MyAccount.as_view(), name='my_account'),
 ]
